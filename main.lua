37d754aff7a58a03713273011e6b5eb838316760
Henrique Gogó <henriquegogo@gmail.com>
2018-03-21 01:49:26 -0300

    16 step

--- main.lua
+++ main.lua
@@ -1,33 +1,33 @@
 function love.load()
   count = 0
   timer = 0
-  steps = 8
+  steps = 16
   bpm = 100
-  ratio = 0.1 / 3 * bpm
+  ratio = 0.1 / 1.5 * bpm
 
   kick  = love.audio.newSource('samples/kick.wav',  'static')
   snare = love.audio.newSource('samples/snare.wav', 'static')
   hihat = love.audio.newSource('samples/hihat.wav', 'static')
   empty = love.audio.newSource('samples/empty.wav', 'static')
 
-  seq = {kick, hihat, snare, hihat, kick, hihat, snare, kick, kick}
+  seq = {kick, empty, hihat, empty, snare, empty, hihat, empty, kick, empty, hihat, empty, snare, empty, kick, empty, kick}
 end
 
 function love.keypressed(key)
   local sample
 
   if key == 'kp1' then
-    sample = kick:clone()
+    sample = kick
   elseif key == 'kp2' then
-    sample = snare:clone()
+    sample = snare
   elseif key == 'kp3' then
-    sample = hihat:clone()
+    sample = hihat
   else
-    sample = empty:clone()
+    sample = empty
   end
 
   seq[(count % steps) + 1] = sample
-  sample:play()
+  sample:clone():play()
 end
 
 function love.update(dt)
@@ -38,7 +38,7 @@ function love.update(dt)
   if count ~= oldcount then
     print('Metronome: ' .. (count % steps) + 1)
 
-    local sample = seq[(count % steps) + 1]:clone()
-    sample:play()
+    local sample = seq[(count % steps) + 1]
+    sample:clone():play()
   end
 end
